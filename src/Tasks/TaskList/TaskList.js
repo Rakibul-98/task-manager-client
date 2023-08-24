eec4b3ea35a32355346cd07e5aac77ea23e5bcbd
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-08-24 10:22:21 +0600

    some codtion added when task list is empty

--- src/Tasks/TaskList/TaskList.js
+++ src/Tasks/TaskList/TaskList.js
@@ -32,23 +32,28 @@ const TaskList = () => {
     return (
         <div className='p-2 bg-red-200 rounded-lg'>
             <p className='py-3'>You have <span className='font-bold text-xl'>{tasks.length}</span> {tasks.length > 1 ? "tasks" : "task"} in hand</p>
-            <ul className='bg-rose-50 py-2 px-5 rounded-b-lg'>
-                {
-                    tasks.map(task =>
-                        <div key={task._id} className='bg-rose-200 my-3 flex justify-between'>
-                            <p className='ps-2 cursor-pointer hover:bg-rose-400 hover:text-white w-full text-left tooltip' onClick={() => handleTaskDetails(task._id)} data-tip="Click to show more">{task.title} : {task.details.length > 30 ? task.details.slice(0, 30) + " ..." : task.details}</p>
-                            <div className='flex'>
-                                <p className={`${task.status ==="Pending"?"bg-yellow-300" :"bg-green-500 text-white"} px-2`}>{task.status}</p>
-                                <button
-                                    className='bg-red-500 px-2 text-white font-semibold hover:bg-red-400 lg:tooltip'
-                                    onClick={() => handleDelete(task._id)}
-                                    data-tip="delete task"
-                                >X</button></div>
-                        </div>
 
-                    )
-                }
-            </ul>
+            {
+                tasks.length ?
+                    <ul className='bg-rose-50 py-2 px-5 rounded-b-lg'>
+                        {
+                            tasks.map(task =>
+                                <div key={task._id} className='bg-rose-200 my-3 flex justify-between'>
+                                    <p className='ps-2 cursor-pointer hover:bg-rose-400 hover:text-white w-full text-left tooltip' onClick={() => handleTaskDetails(task._id)} data-tip="Click to show more"><span className='font-bold uppercase'>{task.title}</span> : {task.details.length > 30 ? task.details.slice(0, 30) + " ..." : task.details}</p>
+                                    <div className='flex'>
+                                        <p className={`${task.status === "Pending" ? "bg-yellow-300" : "bg-green-500 text-white"} px-2`}>{task.status}</p>
+                                        <button
+                                            className='bg-red-500 px-2 text-white font-semibold hover:bg-red-400 lg:tooltip'
+                                            onClick={() => handleDelete(task._id)}
+                                            data-tip="delete task"
+                                        >X</button></div>
+                                </div>
+
+                            )
+                        }
+                    </ul> : <p className='text-error font-semibold text-xl text-center bg-white py-5'>Add task to see tasklist</p>
+            }
+
             <TaskDetails task={taskDetails}></TaskDetails>
         </div>
     );
